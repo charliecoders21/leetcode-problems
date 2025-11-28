be754678877a94418e5b18894dcc48a42ae57111
8204 <cnegi@raa.com.au>
2025-11-28 09:02:02 +0530

    leetcode problem part 3

---
+++ Priority Queue/kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
@@ -0,0 +1,23 @@
+from typing import List
+import heapq
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.min_heap = nums
+        heapq.heapify(self.min_heap)  # Convert the list into a heap
+        # Keep only the k largest elements in the heap
+        while len(self.min_heap) > k:
+            heapq.heappop(self.min_heap)
+
+    def add(self, val: int) -> int:
+        if len(self.min_heap) < self.k:
+            heapq.heappush(self.min_heap, val)
+        elif val > self.min_heap[0]:
+            heapq.heapreplace(self.min_heap, val)
+        return self.min_heap[0]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
